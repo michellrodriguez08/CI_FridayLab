878abd45e4b16efa2c8a07005687a5cdda96487a
Michell <115733586+michellrodriguez08@users.noreply.github.com>
2023-12-08 15:46:00 -0800

    Created under Version Control

---
+++ src/test/java/com/example/diceproject/DiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
